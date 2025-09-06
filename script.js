eda2ce5f8860e15c4ff8bac14d72d99147b9f3d1
Pikime <98193910+Pikime@users.noreply.github.com>
2025-09-06 22:14:35 +1000

    actually equals

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const sketchCell = document.createElement("div");
 
 sketchCell.textContent = "I am a Cell";
 sketchCell.style.height = "100px";
-sketchCell.style.aspect-ratio: "1/1";
+sketchCell.style.aspect-ratio = "1/1";
 
-
-sketchPad.appendChild(sketchCell);
+for (let i = 0; i < 4; i++) {
+    sketchPad.appendChild(sketchCell);
+}
