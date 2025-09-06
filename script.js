1aa9e42152822834e3e919997f0bc2a012260dff
Pikime <98193910+Pikime@users.noreply.github.com>
2025-09-06 22:26:53 +1000

    maybe this will work

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 console.log("Hello World!");
 const sketchPad = document.querySelector("#sketchPad");
 
-const sketchCell = document.createElement("div");
-
-sketchCell.textContent = "I am a Cell";
-sketchCell.style.height = "100px";
-sketchCell.style.aspect-ratio = "1/1";
-
 for (let i = 0; i < 4; i++) {
+    const sketchCell = document.createElement("div");
+    sketchCell.textContent = "I am a Cell";
+    sketchCell.style.height = "100px";
+    sketchCell.style.aspectRatio = "1/1";
     sketchPad.appendChild(sketchCell);
 }
