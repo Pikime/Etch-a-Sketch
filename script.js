d97e2bd9cce1206fa5e6b1b0687073ae5f70a835
Pikime <98193910+Pikime@users.noreply.github.com>
2025-09-06 22:05:03 +1000

    height

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const sketchPad = document.querySelector("#sketchPad");
 const sketchCell = document.createElement("div");
 
 sketchCell.textContent = "I am a Cell";
+sketchCell.style.height = "100px";
 
 sketchPad.appendChild(sketchCell)
