d5cb4d5ae211c782ca8cad20b80aaf73c2d5e133
Pikime <98193910+Pikime@users.noreply.github.com>
2025-09-06 22:49:21 +1000

    vertical now?

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 console.log("Hello World!");
 const sketchPad = document.querySelector("#sketchPad");
 
+let size = math.min(window.innerwidth, window.innerheight);
+
+console.log("the minimum size")
+
 for (let i = 0; i < 4; i++) {
     const sketchCell = document.createElement("div");
     sketchCell.textContent = "I am a Cell";
