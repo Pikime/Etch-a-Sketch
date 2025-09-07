2be3fa7eb103bf130ffdd6290ac0b4924f73fd38
Pikime <98193910+Pikime@users.noreply.github.com>
2025-09-07 10:35:39 +1000

    lets grow

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ sketchPad.style.aspectRatio = "1/1";
 for (let i = 0; i < 4; i++) {
     const sketchRow = document.createElement("div");
     sketchRow.style.display = "flex";
+    sketchRow.style.flexGrow = 1;
     for (let j = 0; j < 4; j++) {
         const sketchCell = document.createElement("div");
         sketchCell.textContent = "I am a Cell";
         //sketchCell.style.height = "100px";
         sketchCell.style.aspectRatio = "1/1";
+        sketchCell.style.flexGrow = 1;
         sketchRow.appendChild(sketchCell);
     }
     sketchPad.appendChild(sketchRow)    
