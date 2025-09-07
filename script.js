335b1ccc769215ad0a3f1c66c0bce2d520c83b8e
Pikime <98193910+Pikime@users.noreply.github.com>
2025-09-07 10:06:38 +1000

    four be four

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ let size = Math.min(window.innerWidth, window.innerHeight);
 
 console.log("the minimum size is: " + size)
 
+sketchPad.style.height = size;
 sketchPad.style.aspectRatio = "1/1";
 
-for (let i = 0; i < 4; i++) {
-    const sketchCell = document.createElement("div");
-    sketchCell.textContent = "I am a Cell";
-    sketchCell.style.height = "100px";
-    sketchCell.style.aspectRatio = "1/1";
-    sketchPad.appendChild(sketchCell);
+for (let i = 0; i < 0; i++) {
+    const sketchRow = document.createElement("div");
+    sketchPad.appendChild(sketchRow)    
+    for (let j = 0; j < 4; j++) {
+        const sketchCell = document.createElement("div");
+        sketchCell.textContent = "I am a Cell";
+        //sketchCell.style.height = "100px";
+        sketchCell.style.aspectRatio = "1/1";
+        sketchRow.appendChild(sketchCell);
+    }
 }
