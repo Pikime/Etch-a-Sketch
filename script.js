79be143c093f6eb0b342e5a39d622a527cc84105
Pikime <98193910+Pikime@users.noreply.github.com>
2025-09-07 10:00:36 +1000

    capitol

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 console.log("Hello World!");
 const sketchPad = document.querySelector("#sketchPad");
 
-let size = Math.min(window.innerwidth, window.innerheight);
+let size = Math.min(window.innerWidth, window.innerHeight);
 
 console.log("the minimum size is: " + size)
+
+sketchPad.style.aspectRatio = "1/1";
 
 for (let i = 0; i < 4; i++) {
     const sketchCell = document.createElement("div");
